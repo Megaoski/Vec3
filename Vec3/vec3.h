5639065e8dba0d78f45bf50b1e27a3f0a8d04ed3
Megaoski <oscar_2596@hotmail.com>
2019-09-17 20:27:28 +0200

    Adding operators

--- Vec3/vec3.h
+++ Vec3/vec3.h
@@ -11,7 +11,59 @@ public:
 	Vec3(Vec3 &vec) : x(vec.x), y(vec.y), z(vec.z) {}//copy constructor
 	~Vec3(){}//destructor
 
-public:
+	//OPERATORS
+	Vec3 operator+  (const vec3 &vec)
+	{
+		vec3 ret;
+		ret.x = x + vec.x;
+		ret.y = y + vec.y;
+		ret.z = z + vec.z;
+		return ret;
+	}
+
+	Vec3 operator-  (const vec3 &vec)
+	{
+		vec3 ret;
+		ret.x = x - vec.x;
+		ret.y = y - vec.y;
+		ret.z = z - vec.z;
+		return ret;
+	}
+
+	void operator+=  (const vec3 &vec)
+	{
+		
+		x += vec.x;
+		y += vec.y;
+		z += vec.z;
+		
+	}
+
+	void operator-=  (const vec3 &vec)
+	{
+
+		x -= vec.x;
+		y -= vec.y;
+		z -= vec.z;
+
+	}
+
+	void operator=  (const vec3 &vec)
+	{
+
+		x = vec.x;
+		y = vec.y;
+		z = vec.z;
+
+	}
+
+	bool operator== (const vec3 &vec)
+	{
+
+		return x == vec.x && y == vec.y && z == vec.z;
+	}
+
+private:
 	TYPE x, y, z;
 };
 
