c0a3386af916952a60a0b067e0c850a95e6010d4
Megaoski <oscar_2596@hotmail.com>
2019-09-18 15:58:22 +0200

    Added methods

--- Vec3/vec3.h
+++ Vec3/vec3.h
@@ -1,6 +1,8 @@
 #ifndef __VEC3__
 #define	__VEC3__
 
+#include <cmath>
+
 template<class TYPE>
 
 class Vec3
@@ -61,6 +63,33 @@ public:
 	{
 
 		return x == vec.x && y == vec.y && z == vec.z;
+	}
+
+
+	//METHODS
+	void normalize()
+	{
+		x = x / sqrt(x*x + y*y + z*z);
+		y = y / sqrt(x*x + y*y + z*z);
+		z = z / sqrt(x*x + y*y + z*z);
+		
+	}
+
+	void zero()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+
+	bool isZero() const
+	{
+		return x == 0 && y == 0 && z == 0;
+	}
+
+	TYPE distanceTo(const vec3 vec)
+	{
+		return sqrt((vec.x - x) ^ 2 + (vec.y - y) ^ 2 + (vec.z - z) ^ 2);
 	}
 
 private:
